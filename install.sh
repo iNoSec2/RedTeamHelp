12f21d667149ba608785b1ea798d2efdc7eb70e1
kyle <kyle41111@gmail.com>
2023-01-20 15:56:44 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
+pip install python-ldap
+sudo apt-get install mingw-w64
+sudo apt install golang
+go get github.com/fatih/color
+go get github.com/yeka/zip
+go get github.com/josephspurrier/goversioninfo
+git clone https://github.com/optiv/ScareCrow.git
+cd ScareCrow
+go build ScareCrow.go
+cd ..
+cd ActiveDirectory
+git clone https://github.com/ropnop/windapsearch.git
+cd ..
